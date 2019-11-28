88adf1412bb01ace1f180b4452b4068fe0f55021
LAUNOIS Remy <remy.launois@IGTP5-PC15.isim.intra>
2019-11-28 15:05:44 +0100

    Rendu finale?

--- main.swift
+++ main.swift
@@ -1,91 +1,5 @@
-// Déroulement du programe principal 
-func Partie(){
-	// Initialisation de la partie 
-	var jeu : Jeu = Jeu()
-	var estFini : Bool = false
 
-	print(" le ",jeu.jCourant, " commence")
-
-	while !estFini {
-		// Affichage du jeu chaque tour
-		afficher(jeuToString( j : jeu))
-		jeu.Afficher()
-		var aJoue = false
-
-		// Boucle pour réaliser un tour de jeu 
-		while (!aJoue){
-			// Affichage du joueur courant et de ses pièces restantes
-			print("Le joueur : " , jeu.jCourant.couleur ," place sa pièce " )
-			print (" Les pièces disponibles sont : ", jeu.jCourant.PieceRestantes())
-
-			// Choix de la pièce
-			print("Quel pièce voulez-vous choisir ? (ce : Cercle - ca : Carre - cy : Cylindre - tr : Triangle")
-			var pieceOk : Bool = false
-
-			var pieceString : String = ""
-			//Boucle qui vérifie si le string en entrée (readLine) est correct 
-			while(!pieceOk){
-				if let _piece = readLine() {
-					
-					let _piece = _piece.lowercased()
-					if( _piece == "ca" || _piece == "ce" || _piece == "cy" || _piece == "tr"){
-						pieceOk = true
-						pieceString  = _piece
-					}			
-				}else {
-		    		print(" Le nom de la piece doit être ca,ce,cy ou tr. veuillez retaper : ")
-		    	}
-			}
-			var piece : Piece = stringToPiece(nom : pieceString )
-
-			// Choix de la coordonnée x
-			print("Coordonnées x : ")
-			let x = lireNombre()
-
-			// Choix de la coordonnée y
-			print("Coordonnées y : ")
-			let y = lireNombre()
-
-			// Vérifie si le joueur peut placer sa piece 
-			var peutPlacer : Bool  = jeu.PeutPlacer(j : jeu.jCourant, p : piece, x : x, y : y)
-
-			// Place la piece si c'est possible et s'il confirme
-			if(peutPlacer) {
-				print ("Confirmer le placement : ", piece , " ,x : " , x , " ,y : " , y , " ? ( oui/o ou autre pour non")
-				if let ok = readLine() {
-					let ok = ok.lowercased()
-
-					if( piece == "oui" || piece == "o" ){
-						jeu.Placer(j : jeu.jCourant, p : piece, x : x, y : y)
-						aJoue = true 
-
-						// Vérification si la partie est finie
-						if (jeu.estFini(x : x, y : y) ) {
-							print(jeu.jCourant , " a gagné !!!!!!")
-							estFini = true
-						}
-						// Change de joueur si le jeu n'est pas fini 
-						else { 
-							let jprec : Joueur = jeu.jCourant
-							jeu.joueurSuivant()
-							// Vérifie que le nouveau joueur peut jouer sinon le jeu est fini 
-							if( !jeu.jCourant.peutJouer(jeu : jeu)) {
-								print(jprec , " a gagné !!!!!!")
-							}
-						}
-					}	
-				}
-			}
-			// Si le joueur n'a pas pu placer sa pièce retour en boucle aJoue
-			else {
-				print("Parametre incorrect ")
-			}
-
-		}// Fin de boucle aJoue
- 
-	}// Fin de boucle estFini  
-}
-
+// ============================ Fonctions utilitaires =================================================
 // Fonction qui permet de lire un entier 
 // Post condition : l'entier est entre 0 et 3
 func lireNombre() -> Int {
@@ -114,7 +28,7 @@ func jeuToString(j : Jeu) -> [[String]]{
 	var tab : [[String]] = [["-","-","-","-"],["-","-","-","-"],["-","-","-","-"],["-","-","-","-"]]
 	for x in 0...3{
 		for y in 0...3 {
-			var pTmp : Piece? = j.getPiece(x : x ,y : y)
+			let pTmp : Piece? = j.getPiece(x : x ,y : y)
 			if let piece = pTmp {
 				let couleur : String = piece.couleur
 				if piece.nom == "tr"{
@@ -149,4 +63,94 @@ func afficher( tab : [[String]])  {
 		}
 		print(message)
 }
+
+// ============================ Programme principal =================================================
+// Déroulement du programe principal 
+
+// Initialisation de la partie 
+var jeu : Jeu = Jeu()
+var estFini : Bool = false
+
+print(" le ",jeu.jCourant, " commence")
+
+while !estFini {
+	// Affichage du jeu chaque tour
+	afficher(tab : jeuToString( j : jeu))
+	var aJoue = false
+
+	// Boucle pour réaliser un tour de jeu 
+	while (!aJoue){
+		// Affichage du joueur courant et de ses pièces restantes
+		print("Le joueur : " , jeu.jCourant.couleur ," place sa pièce " )
+		print (" Les pièces disponibles sont : ", jeu.jCourant.PieceRestantes)
+
+
+		// Choix de la pièce
+		print("Quel pièce voulez-vous choisir ? (ce : Cercle - ca : Carre - cy : Cylindre - tr : Triangle")
+		var pieceOk : Bool = false
+
+		var pieceString : String = ""
+		//Boucle qui vérifie si le string en entrée (readLine) est correct 
+		while(!pieceOk){
+			if let _piece = readLine() {
+				
+				let _piece = _piece.lowercased()
+				if( _piece == "ca" || _piece == "ce" || _piece == "cy" || _piece == "tr"){
+					pieceOk = true
+					pieceString  = _piece
+				}			
+			}else {
+	    		print(" Le nom de la piece doit être ca,ce,cy ou tr. veuillez retaper : ")
+	    	}
+		}
+		let piece : Piece = jeu.stringToPiece(nom : pieceString )
+
+		// Choix de la coordonnée x
+		print("Coordonnées x : ")
+		let x = lireNombre()
+
+		// Choix de la coordonnée y
+		print("Coordonnées y : ")
+		let y = lireNombre()
+
+		// Vérifie si le joueur peut placer sa piece 
+		let peutPlacer : Bool  = jeu.PeutPlacer(j : jeu.jCourant, p : piece, x : x, y : y)
+
+		// Place la piece si c'est possible et s'il confirme
+		if(peutPlacer) {
+			print ("Confirmer le placement : ", piece , " ,x : " , x , " ,y : " , y , " ? ( oui/o ou autre pour non")
+			if let ok = readLine() {
+				let ok = ok.lowercased()
+
+				if( ok == "oui" || ok == "o" ){
+					jeu.Placer(j : jeu.jCourant, p : piece, x : x, y : y)
+					aJoue = true 
+
+					// Vérification si la partie est finie
+					if (jeu.estFini(x : x, y : y) ) {
+						print(jeu.jCourant , " a gagné !!!!!!")
+						estFini = true
+					}
+					// Change de joueur si le jeu n'est pas fini 
+					else { 
+						let jprec : Joueur = jeu.jCourant
+						jeu.joueurSuivant()
+						// Vérifie que le nouveau joueur peut jouer sinon le jeu est fini 
+						if( !jeu.jCourant.peutJouer(jeu : jeu)) {
+							print(jprec , " a gagné !!!!!!")
+						}
+					}
+				}	
+			}
+		}
+		// Si le joueur n'a pas pu placer sa pièce retour en boucle aJoue
+		else {
+			print("Parametre incorrect ")
+		}
+
+	}// Fin de boucle aJoue
+
+}// Fin de boucle estFini  
+
+
 
